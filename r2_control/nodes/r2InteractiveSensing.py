34cad2e80b5e044ad3383c402bff22b316227110
Steve Hart <stephen.hart@nasa.gov>
2014-04-16 17:54:09 -0500

    move stuff around some more

--- r2_control/nodes/r2InteractiveSensing.py
+++ r2_control/nodes/r2InteractiveSensing.py
@@ -17,8 +17,8 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 """
 
-import roslib; 
-roslib.load_manifest("r2_teleop")
+import roslib;
+roslib.load_manifest("r2_control")
 import rospy
 import math
 import copy
@@ -63,8 +63,8 @@ need_to_orient = False
 X = np.array([1, 0, 0])
 Y = np.array([0, 1, 0])
 Z = np.array([0, 0, 1])
-	 		
-	 		
+
+
 def getInteractiveMarkerID(name) :
 	for i in range(len(interactive_marker_list)) :
 		if name == interactive_marker_list[i].name :
@@ -89,11 +89,11 @@ def TrotZ(theta):
 	return T
 
 def parseObjectPose(idx):
-	
+
 	print "parsing object pose"
 	now = rospy.Time(0)
 	pose = PoseStamped()
-	
+
 	frame_id = interactive_marker_list[idx].controls[0].markers[0].header.frame_id
 	marker = interactive_marker_list[idx].controls[0].markers[0]
 	scale = (marker.scale.x, marker.scale.y, marker.scale.z)
@@ -106,9 +106,9 @@ def parseObjectPose(idx):
 	tf_listener.waitForTransform(robot_frame, frame_id, now, rospy.Duration(2.0))
     #(trans, rot) = tf_listener.lookupTransform(frame_id, robot_frame, now)
 
-	pose.header = interactive_marker_list[idx].controls[0].markers[0].header 
+	pose.header = interactive_marker_list[idx].controls[0].markers[0].header
 	pose.pose = interactive_marker_list[idx].controls[0].markers[0].pose
-		
+
 	pose.header.stamp = now
 	pose = tf_listener.transformPose(robot_frame, pose)
 	print "transformed pose: "
@@ -121,11 +121,11 @@ def parseObjectPose(idx):
 	#obj = obj_transforms[idx]
 	#T = pm.fromTf((obj[0],obj[1]))
 	#t = pm.toTf(T)
-	
+
 	# get rotational and component parts of frame
 	#R = T.M
 	#p = T.p
-	
+
 	if p[1] > 0:
 		arm = "right"
 	else :
@@ -157,19 +157,19 @@ def reachToObject(idx) :
 	x_off = -0.085
 	y_off = 0.075
  	z_off = 0.12
-	
+
 	marker = interactive_marker_list[idx].controls[0].markers[0]
-	dim = np.array([marker.scale.x, marker.scale.y, marker.scale.z]) 
+	dim = np.array([marker.scale.x, marker.scale.y, marker.scale.z])
 	poseOff = copy.deepcopy(pose)
 
 	if scale[0] > scale[1] :
 		max_side_dim = 0
 	else :
-		max_side_dim = 1       
+		max_side_dim = 1
 	x_reach_off = x_off
 	z_reach_off = scale[2]+z_off
 	y_reach_off = scale[max_side_dim]+y_off # should be scaled by side projected width (not just max side width)
-        
+
 	if approach == "side" :
 		if arm == "left" :
 		    q = (kdl.Rotation.RPY(-1.35,0,0)).GetQuaternion()
@@ -192,7 +192,7 @@ def reachToObject(idx) :
 
 	if approach == "top" :
 		poseOff.pose.position.z -= z_reach_off
-	else: 	
+	else:
 		poseOff.pose.position.y += y_reach_off
 
 	print "final poseOff: "
@@ -203,7 +203,7 @@ def reachToObject(idx) :
 	else :
 		r2.right_pose_pub.publish(poseOff)
 
-	# wait to finish reaching	
+	# wait to finish reaching
 	rospy.sleep(4.0)
 
 	# do grasp approach
@@ -215,12 +215,12 @@ def reachToObject(idx) :
 		graspPose.pose.orientation.y = q[1]
 		graspPose.pose.orientation.z = q[2]
 		graspPose.pose.orientation.w = q[3]
-	
+
 	graspPose.header.stamp = rospy.get_rostime()
-	
+
 	if approach == "top" :
 		graspPose.pose.position.z -= z_reach_off*0.1
-	else: 
+	else:
 		graspPose.pose.position.y += y_reach_off*0.0
 
 	if arm == "left" :
@@ -228,7 +228,7 @@ def reachToObject(idx) :
 	else :
 		r2.right_pose_pub.publish(graspPose)
 
-	# wait to finish reaching	
+	# wait to finish reaching
 	rospy.sleep(2.0)
 
 	if arm == "left" :
@@ -239,12 +239,12 @@ def reachToObject(idx) :
 		r2.right_jnt_pub.publish(r2.rightHandPowerClose)
 		rospy.sleep(0.3)
 		r2.right_jnt_pub.publish(r2.rightHandPowerCloseThumb)
-		
-	# wait to finish reaching	
+
+	# wait to finish reaching
 	rospy.sleep(1.5)
 
 	liftPose = copy.deepcopy(graspPose)
-	liftPose.pose.position.z -= 0.15	
+	liftPose.pose.position.z -= 0.15
 
 	if arm == "left" :
 		r2.left_pose_pub.publish(liftPose)
@@ -253,18 +253,18 @@ def reachToObject(idx) :
 
 
 def handleObjectMenu( feedback ) :
-	
+
 	handle = feedback.menu_entry_id
 	idx = getInteractiveMarkerID(feedback.marker_name)
 	print "Pick Me Up call on object: ", feedback.marker_name
-	
+
 	if(handle == 1) : # pick me up
 		reachToObject(idx)
 	elif(handle == 2) : # parse
 		parseObjectPose(idx)
-	
+
 def processFeedback( feedback ) :
-         
+
     s = "Feedback from marker '" + feedback.marker_name
     s += "' / control '" + feedback.control_name + "'"
 
@@ -281,13 +281,13 @@ def processFeedback( feedback ) :
     elif feedback.event_type == InteractiveMarkerFeedback.MENU_SELECT:
         rospy.loginfo( s + ": menu item " + str(feedback.menu_entry_id) + " clicked" + mp + "." + "marker_name: " + feedback.marker_name  )
         handleObjectMenu(feedback)
-        
+
     #elif feedback.event_type == InteractiveMarkerFeedback.POSE_UPDATE:
     #    poseUpdate(feedback)
 
     #elif feedback.event_type == InteractiveMarkerFeedback.MOUSE_DOWN:
     #    rospy.loginfo( s + ": mouse down" + mp + "." )
-    
+
     #elif feedback.event_type == InteractiveMarkerFeedback.MOUSE_UP:
     #    mouseUpdate(feedback)
 
@@ -306,20 +306,20 @@ def marker_array_cb(data) :
 
 	rospy.sleep(0.25)
 
-	marker_data = data	
+	marker_data = data
 	print "got new marker array of size: ", len(data.markers)
 	#print marker_data
 
 	for m in range(len(marker_data.markers)) :
-		
+
 		interactive_marker_list.append(InteractiveMarker())
-		interactive_marker_list[m].header.frame_id = "r2/asus_frame" 
+		interactive_marker_list[m].header.frame_id = "r2/asus_frame"
 		interactive_marker_list[m].scale = 1#0.2
 		interactive_marker_list[m].name = "obj_"+str(m)
 		interactive_marker_list[m].description = "obj_"+str(m)
 		control = InteractiveMarkerControl()
 		control.interaction_mode = InteractiveMarkerControl.MENU
-		
+
 		marker = marker_data.markers[m]
 		marker.color.r = 1
 		marker.color.g = 0
@@ -331,30 +331,30 @@ def marker_array_cb(data) :
 		obj_menu_handler.apply( server, interactive_marker_list[m].name )
 		server.applyChanges()
 
-		pose.header = interactive_marker_list[m].controls[0].markers[0].header 
+		pose.header = interactive_marker_list[m].controls[0].markers[0].header
 		pose.pose = interactive_marker_list[m].controls[0].markers[0].pose
 		pose = tf_listener.transformPose(robot_frame, pose)
-		
+
 		p = (pose.pose.position.x, pose.pose.position.y, pose.pose.position.z)
 		q = (pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z, pose.pose.orientation.w)
 		obj_transforms.append((p, q, interactive_marker_list[m].name))
-		
+
 		need_to_orient = True
-		
+
 def SetUpObjectMenu() :
-	
+
 	obj_menu_handler.insert( "Pick Up",  callback=processFeedback )
 	obj_menu_handler.insert( "Parse Object",  callback=processFeedback )
 
 if __name__=="__main__":
 
-    rospy.init_node("r2_interactive_sensing")    
-    server = InteractiveMarkerServer("r2_interactive_sensing")
+    rospy.init_node("r2InteractiveSensing")
+    server = InteractiveMarkerServer("r2InteractiveSensing")
     tf_listener = tf.TransformListener()
     tf_broadcaster = tf.TransformBroadcaster()
-    
+
     need_to_orient = False
-    
+
     # Subscribe to marker data
     rospy.Subscriber("/tabletop_clustering/vis_marker", MarkerArray, marker_array_cb)
 
@@ -362,21 +362,21 @@ if __name__=="__main__":
     SetUpObjectMenu()
 
     server.applyChanges()
-    
-    print "in r2_interactive_sensing"
+
+    print "in r2InteractiveSensing"
 #	rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
-        
+
         if need_to_orient :
             need_to_orient = False
 	    print "got objects"
 	    for i in range(len(obj_transforms)) :
                 obj = obj_transforms[i]
-	     		
+
                 T = pm.fromTf((obj[0],obj[1]))
                 #T = T*TrotX(90*TORAD)*TrotY(270*TORAD)
-                t = pm.toTf(T)	     			     		
-                tf_broadcaster.sendTransform(t[0], t[1], rospy.Time.now(), obj[2], robot_frame) 
+                t = pm.toTf(T)
+                tf_broadcaster.sendTransform(t[0], t[1], rospy.Time.now(), obj[2], robot_frame)
                 q = T.M.GetQuaternion()
                 obj_transforms[i] = (t[0], q, interactive_marker_list[i].name)
                 rospy.sleep(0.5)
